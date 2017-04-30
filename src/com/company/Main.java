cebf590de8f69b462d94462f7f2acc80341d3840
AlexTsybin <alexander.tsybin@gmail.com>
2017-04-30 22:13:42 +0300

    Add new class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,18 @@
 package com.company;
 
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
 	    String url = "http://javadevblog.com/sitemap.xml";
         System.out.println(url);
+        try {
+            Download.downloadUsingStream(url, "/Users/os_mac/IdeaProjects/JavaExam/map.xml");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+
 }
