e368e9d68a80af74c90d3495305784d52af7ca09
AlexTsybin <alexander.tsybin@gmail.com>
2017-05-01 23:09:40 +0300

    Add threads

--- src/com/company/Download.java
+++ src/com/company/Download.java
@@ -8,18 +8,31 @@ import java.net.URL;
 /**
  * Created by os_mac on 30.04.17.
  */
-public class Download {
-    public static void downloadUsingStream(String urlStr, String file) throws IOException {
-        URL url = new URL(urlStr);
-        BufferedInputStream bis = new BufferedInputStream(url.openStream());
-        FileOutputStream fis = new FileOutputStream(file);
-        byte[] buffer = new byte[1024];
-        int count=0;
-        while((count = bis.read(buffer,0,1024)) != -1)
-        {
-            fis.write(buffer, 0, count);
-        }
-        fis.close();
-        bis.close();
+public class Download extends Thread {
+    String url;
+    String outFile;
+    public Download(String url, String outFile){
+        this.url = url;
+        this.outFile = outFile;
     }
+    public void run(){
+        try {
+            URL urlcon = new URL(url);
+            BufferedInputStream bis = new BufferedInputStream(urlcon.openStream());
+            FileOutputStream fis = new FileOutputStream(outFile);
+            byte[] buffer = new byte[1024];
+            int count=0;
+            while((count = bis.read(buffer,0,1024)) != -1)
+            {
+                fis.write(buffer, 0, count);
+            }
+            fis.close();
+            bis.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    //public static void downloadUsingStream(String urlStr, String file) throws IOException {
+
+    //}
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,12 +11,16 @@ public class Main {
         strings[0] = "http://www.licey86.ru/content/galleryBig/file204.jpg";
         strings[1] = "http://www.licey86.ru/content/galleryBig/file34.jpg";
         strings[2] = "http://www.licey86.ru/content/galleryBig/file19.jpg";
-        try {
-            for (int i = 0; i < strings.length; i++){
-                Download.downloadUsingStream(strings[i], "/Users/os_mac/IdeaProjects/JavaExam/" + "lic" + (i + 1) + ".jpg");
+
+        for (int i = 0; i < strings.length; i++){
+            Download download = new Download(strings[i], "/Users/os_mac/IdeaProjects/JavaExam/" + "lic" + (i + 1) + ".jpg");
+            download.start();
+            try {
+                download.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+            //Download.downloadUsingStream(strings[i], "/Users/os_mac/IdeaProjects/JavaExam/" + "lic" + (i + 1) + ".jpg");
         }
     }
 
